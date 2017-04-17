c2491d700187c39af1db0c48a01a0e58a8085e72
Gus Crawford <gus.crawford@live.ca>
2017-04-17 01:00:24 -0400

    work on reszising

--- clovus-ui/components/ResizeableViewPanel.component.ts
+++ clovus-ui/components/ResizeableViewPanel.component.ts
@@ -6,34 +6,45 @@ import * as ng from 'angular';
  * 
  */
 enum Sides {
-    Left,
-    Top,
-    Right,
-    Bottom
+    none,
+    left,
+    top,
+    right,
+    bottom
 }
 export class ResizeableViewPanel {
     public static $inject = ['$element', '$scope'];
-    
+    public static edgeSize : number = 15;
     constructor (
         private $element : ng.IRootElementService,
         private $scope : ng.IScope
                 ) {
-        this.$element.on('mousedown', this.handleMouseDown(this));
+        this.eventHash = {
+            mousedown :  this.handleMouseDown(this),
+            mouseup: this.handleMouseUp(this)
+        };
+        for (var key in this.eventHash) this.$element.on(key,(this.eventHash as any)[key]);
         this.mouse = new Models.Location();
         this.size = new Models.Size();
         this.size.units = Models.Units.px;
+
+        var elm = this.$element[0];
+        elm.style.left = elm.style.right = elm.style.top = elm.style.bottom = "0";
     }
 
+    private eventHash : any;
     public size : Models.Size;
     public mouse : Models.Location;
     
     resize(side : Sides, value : number, units : Models.Units = Models.Units.px) {
         var elm = this.$element[0],
-            currentStyle = elm.style[(Sides[side] as any).toCamelCase()].match(/\d+/),
+            currentStyle = elm.style[Sides[side] as any].match(/\d+/),
             current = currentStyle ? parseInt(currentStyle[0],10):0;
-        elm.style[(Sides[side] as any).toCamelCase()] = String(current + value) + Models.Units[units];
+        elm.style[Sides[side] as any] = String(current + value) + Models.Units[units];
     }
-
+    handleMouseUp (ctrl : ResizeableViewPanel) { return (event : any)=> {
+        ctrl.$element.off('mousemove', ctrl.eventHash.mousemove);
+    } }
     handleMouseDown (ctrl : ResizeableViewPanel) { return (event : any)=> {
         var elm = ctrl.$element[0];
         ctrl.mouse.x = event.offsetX;
@@ -41,8 +52,33 @@ export class ResizeableViewPanel {
         ctrl.size.width = elm.offsetWidth;
         ctrl.size.height = elm.offsetHeight;
         ctrl.$scope.$apply();
-        ctrl.resize(Sides.Left, 1);
+        var side : Sides = ctrl.onEdge(ctrl);
+        if (side) {
+            ctrl.eventHash.mousemove = ctrl.handleMouseMove(ctrl, side);
+            ctrl.$element.on('mousemove', ctrl.eventHash.mousemove);
+        } 
+        else ctrl.$element.off('mousemove', ctrl.eventHash.mousemove);
     } }
+    onEdge(ctrl : ResizeableViewPanel) :Sides {
+        if (ctrl.mouse.x < ResizeableViewPanel.edgeSize) {
+            return Sides.left;
+        }
+        if (ctrl.mouse.y < ResizeableViewPanel.edgeSize) {
+            return Sides.top;
+        }
+        if (ctrl.size.width - ctrl.mouse.x < ResizeableViewPanel.edgeSize) {
+            return Sides.right;
+        }
+        if (ctrl.size.height - ctrl.mouse.y < ResizeableViewPanel.edgeSize) {
+            return Sides.bottom;
+        }
+        return Sides.none;
+    }
+    handleMouseMove (ctrl : ResizeableViewPanel, side : Sides) { return (event : any)=> {
+        var elm = ctrl.$element[0];
+        ctrl.resize(side, side % 2 ? event.movementX : event.movementY);
+        ctrl.$scope.$apply();
+    } }    
 }
 ClovusUi.ngModule.component((ResizeableViewPanel as any).name.toCamelCase(), {
     controller: ResizeableViewPanel,
